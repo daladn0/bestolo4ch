9ff9b1f07629531aa8b39ca81e04fa4efef3a8af
daladn0 <markkostevychx@gmail.com>
2020-05-12 21:34:27 +0300

    added audio, some effects, fixed bugs and a little js

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,25 @@
 $(document).ready(function(){
+    // ScrollToTop
+    $(window).scroll(function () {
+      if ($(this).scrollTop() > 1200) {
+        $('.pageup').fadeIn();
+      } else {
+        $('.pageup').fadeOut();
+      }
+    });
+
+    $("a[href=#up]").click(function () {
+      const _href = $(this).attr("href");
+      $("html, body").animate({
+        scrollTop: $(_href).offset().top + "px"
+      }, 600);
+      return false;
+    });
 
     // Carousel
     $('.cards').owlCarousel({
-        // autoplay: true,
-        // autoplayTimeout: 10000,
+        autoplay: true,
+        autoplayTimeout: 10000,
         smartSpeed: 500,
         margin: 25,
         responsive:{
@@ -20,8 +36,16 @@ $(document).ready(function(){
     // Audio
     let playButtons = document.querySelectorAll('#play-button');
     
-    playButtons.forEach(item => item.addEventListener('click', function(){
-      this.querySelector('audio').play();
+    playButtons.forEach(item => item.addEventListener('click', function(e){
+      e.preventDefault();
+      if(this.querySelector('audio').paused) {
+        this.querySelector('audio').play();
+      } else {
+        this.querySelector('audio').pause();
+      }
     }));
-    
+
+    // Effects
+    $('.promo__quote').slideDown(500);
+
 });
